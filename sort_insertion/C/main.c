a77d05a2a231dd9859a522201cbab2825d27310c
KeN1427 <ken.t.csai@gmail.com>
2020-10-15 00:49:17 +0900

    main.cの修正
    コマンドライン引数で型と昇降順とサイズ（または文字列）を入力することで、挿入ソートを行うようにした。

--- sort_insertion/C/main.c
+++ sort_insertion/C/main.c
@@ -7,41 +7,68 @@
 void	errmsg_and_exit(void);
 
 int		main(int argc, char **argv) {
-	if (argc != 3)
+
+	if (argc != 4)
 		errmsg_and_exit();
 
-	int i;
-	int	flag = atoi(argv[1]);
-	int size = atoi(argv[2]);
-	int tab[size];
+	int		i;
+	char	*type = argv[1];
+	int		flag = atoi(argv[2]);
+	int		size = 0;
+
+	if (!(flag == 0 || flag == 1))
+		errmsg_and_exit();
+
+	puts("===== insertoin sorting =====");
 	
-	if (!(flag == 0 || flag == 1) || size < 1)
-		errmsg_and_exit();
+	if (strcmp(type, "int") == 0) {
+		size = atoi(argv[3]);
+		
+		if (size < 1)
+			errmsg_and_exit();
+		
+		int tab[size];
+		srand((unsigned)time(NULL));
+		
+		printf("bofore: tab = { ");
+		for (i = 0; i < size; i++) {
+			tab[i] = rand() % size + 1;
+			printf("%d ", tab[i]);
+		}
+		printf("}\n");
 
-	srand((unsigned)time(NULL));
-	printf("bofore: tab = { ");
-	for (i = 0; i < size; i++) {
-		tab[i] = rand() % size + 1;
-		printf("%d ", tab[i]);
+		if (flag)
+			int_insertion(tab, size, descending);
+		else
+			int_insertion(tab, size, ascending);
+		
+		printf("after : tab = { ");
+		for (i = 0; i < size; i++)
+			printf("%d ", tab[i]);
+		printf("}\n");
 	}
-	printf("}\n");
-
-	if (flag)
-		int_insertion(tab, size, descending);
+	else if (strcmp(type, "string") == 0) {
+		printf("before: string = %s\n", argv[3]);
+		
+		if (flag)
+			str_insertion(argv[3], descending);
+		else
+			str_insertion(argv[3], ascending);
+	
+		printf("after : string = %s\n", argv[3]);
+	}
 	else
-		int_insertion(tab, size, ascending);
+		errmsg_and_exit();
+
+	puts("=============================");
 	
-	printf("after : tab = { ");
-	for (i = 0; i < size; i++) {
-		printf("%d ", tab[i]);
-	}
-	printf("}\n");
 	return 0;
 }
 
 void	errmsg_and_exit(void) {
 	puts("ERROR: Invalid arguments.");
-	puts("first argument : ascending(0) or descending(1)");
-	puts("second argument: tab size (bigger than 1)");
+	puts("first argument : the type of array ('int' or 'string')");
+	puts("second argument: ascending('0') or descending('1')");
+	puts("third argument : [int] tab size (bigger than 1) / [string] string before sorting");
 	exit(1);
 }
